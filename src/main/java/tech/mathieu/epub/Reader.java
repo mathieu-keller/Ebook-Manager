5d9252b0b93be0a93a95758b712b83ee60268064
Mathieu Keller <business@mathieu.tech>
2022-10-14 13:06:51 +0200

    use lib to scale and compress jpg

--- src/main/java/tech/mathieu/epub/Reader.java
+++ src/main/java/tech/mathieu/epub/Reader.java
@@ -1,5 +1,6 @@
 package tech.mathieu.epub;
 
+import net.coobird.thumbnailator.Thumbnails;
 import tech.mathieu.epub.container.Container;
 import tech.mathieu.epub.opf.Item;
 import tech.mathieu.epub.opf.Opf;
@@ -30,126 +31,118 @@ import java.util.zip.ZipInputStream;
 @ApplicationScoped
 public class Reader {
 
-    public Epub read(InputStream ebook) throws IOException {
-        var book = ebook.readAllBytes();
-        String opfPath;
-        try (var in = new ByteArrayInputStream(book)) {
-            opfPath = getOpfPath(in);
-        }
-        if (opfPath == null) {
-            return null;
-        }
-        Opf opf;
-        try (var in = new ByteArrayInputStream(book)) {
-            opf = getPackage(in, opfPath);
-        }
-        if (opf == null) {
-            return null;
-        }
-        byte[] cover;
-        try (var in = new ByteArrayInputStream(book)) {
-            cover = getCover(opf, opfPath, in);
-        }
-        return new Epub(opf, cover);
+  public Epub read(InputStream ebook) throws IOException {
+    var book = ebook.readAllBytes();
+    String opfPath;
+    try (var in = new ByteArrayInputStream(book)) {
+      opfPath = getOpfPath(in);
     }
-
-    private double determineImageScale(int sourceWidth, int sourceHeight, int targetWidth) {
-        return (double) targetWidth / sourceWidth;
+    if (opfPath == null) {
+      return null;
     }
-
-    byte[] resizeImage(BufferedImage originalImage, int targetWidth) throws IOException {
-        var scale = determineImageScale(originalImage.getWidth(), originalImage.getHeight(), targetWidth);
-        var calculatedWidth = (int) (originalImage.getWidth() * scale);
-        var calculatedHeight = (int) (originalImage.getHeight() * scale);
-        BufferedImage resizedImage = new BufferedImage(calculatedWidth, calculatedHeight, BufferedImage.TYPE_INT_RGB);
-        Graphics2D graphics2D = resizedImage.createGraphics();
-        graphics2D.drawImage(originalImage, 0, 0, calculatedWidth, calculatedHeight, null);
-        graphics2D.dispose();
-
-        ImageOutputStream ios =  ImageIO.createImageOutputStream(resizedImage);
-        Iterator<ImageWriter> iter = ImageIO.getImageWritersByFormatName("jpeg");
-        ImageWriter writer = iter.next();
-        ImageWriteParam iwp = writer.getDefaultWriteParam();
-        iwp.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-        iwp.setCompressionQuality(0.3f);
-
-        writer.setOutput(ios);
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        writer.setOutput(new MemoryCacheImageOutputStream(baos));
-        writer.write(null, new IIOImage(resizedImage,null,null),iwp);
-        writer.dispose();
-        return Base64.getEncoder().encode(baos.toByteArray());
+    Opf opf;
+    try (var in = new ByteArrayInputStream(book)) {
+      opf = getPackage(in, opfPath);
     }
+    if (opf == null) {
+      return null;
+    }
+    byte[] cover;
+    try (var in = new ByteArrayInputStream(book)) {
+      cover = getCover(opf, opfPath, in);
+    }
+    return new Epub(opf, cover);
+  }
 
-    private byte[] getCover(Opf opf, String opfPath, InputStream ebook) throws IOException {
-        var path = getCoverPath(opf);
-        if (path == null) {
-            return null;
+  private double determineImageScale(int sourceWidth, int targetWidth) {
+    return (double) targetWidth / sourceWidth;
+  }
+
+  byte[] resizeImage(BufferedImage originalImage, int targetWidth) throws IOException {
+    var scale = determineImageScale(originalImage.getWidth(), targetWidth);
+    var calculatedWidth = (int) (originalImage.getWidth() * scale);
+    var calculatedHeight = (int) (originalImage.getHeight() * scale);
+    try (var baos = new ByteArrayOutputStream()) {
+      Thumbnails.of(originalImage)
+          .size(calculatedWidth, calculatedHeight)
+          .outputFormat("JPEG")
+          .outputQuality(0.5f)
+          .toOutputStream(baos);
+      return Base64.getEncoder().encode(baos.toByteArray());
+    }
+  }
+
+  private byte[] getCover(Opf opf, String opfPath, InputStream ebook) throws IOException {
+    var path = getCoverPath(opf);
+    if (path == null) {
+      return null;
+    }
+    try (var zipIn = new ZipInputStream(ebook)) {
+      ZipEntry zipEntry;
+      while ((zipEntry = zipIn.getNextEntry()) != null) {
+        var rootFolder = getRootPath(opfPath);
+        if (path.equals(zipEntry.getName()) || (rootFolder + "/" + path).equals(zipEntry.getName())) {
+          try (var baos = new ByteArrayInputStream(zipIn.readAllBytes())) {
+            return resizeImage(ImageIO.read(baos), 270);
+          }
         }
-        try (var zipIn = new ZipInputStream(ebook)) {
-            ZipEntry zipEntry;
-            while ((zipEntry = zipIn.getNextEntry()) != null) {
-                var rootFolder = getRootPath(opfPath);
-                if (path.equals(zipEntry.getName()) || (rootFolder + "/" + path).equals(zipEntry.getName())) {
-                    return resizeImage(ImageIO.read(new ByteArrayInputStream(zipIn.readAllBytes())), 270);
-                }
-            }
-        } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
-            throw e;
-        }
-        return null;
+      }
+    } catch (RuntimeException e) {
+      System.out.println(e.getMessage());
+      throw e;
     }
+    return null;
+  }
 
-    private String getRootPath(String opfPath) {
-        return Arrays.stream(opfPath.split("/"))
-                .filter(element -> !element.endsWith(".opf"))
-                .collect(Collectors.joining("/"));
-    }
+  private String getRootPath(String opfPath) {
+    return Arrays.stream(opfPath.split("/"))
+        .filter(element -> !element.endsWith(".opf"))
+        .collect(Collectors.joining("/"));
+  }
 
-    private String getCoverPath(Opf epub) {
-        if (epub.getMetadata().getMeta() == null) {
-            return null;
-        }
-        var coverPath = epub.getMetadata().getMeta()
-                .stream()
-                .filter(meta -> "cover".equalsIgnoreCase(meta.getName()))
-                .map(Meta::getContent)
-                .map(id -> epub.getManifest().getItems().stream().filter(item -> id.equalsIgnoreCase(item.getId())).findFirst())
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .map(Item::getHref)
-                .findFirst();
-        return coverPath.orElse(null);
+  private String getCoverPath(Opf epub) {
+    if (epub.getMetadata().getMeta() == null) {
+      return null;
     }
+    var coverPath = epub.getMetadata().getMeta()
+        .stream()
+        .filter(meta -> "cover".equalsIgnoreCase(meta.getName()))
+        .map(Meta::getContent)
+        .map(id -> epub.getManifest().getItems().stream().filter(item -> id.equalsIgnoreCase(item.getId())).findFirst())
+        .filter(Optional::isPresent)
+        .map(Optional::get)
+        .map(Item::getHref)
+        .findFirst();
+    return coverPath.orElse(null);
+  }
 
-    private Opf getPackage(InputStream ebook, String opfPath) throws IOException {
-        Opf pa = null;
-        try (var zipIn = new ZipInputStream(ebook)) {
-            ZipEntry zipEntry;
-            while ((zipEntry = zipIn.getNextEntry()) != null) {
-                if (opfPath.equals(zipEntry.getName())) {
-                    pa = JAXB.unmarshal(new ByteArrayInputStream(zipIn.readAllBytes()), Opf.class);
-                    break;
-                }
-            }
+  private Opf getPackage(InputStream ebook, String opfPath) throws IOException {
+    Opf pa = null;
+    try (var zipIn = new ZipInputStream(ebook)) {
+      ZipEntry zipEntry;
+      while ((zipEntry = zipIn.getNextEntry()) != null) {
+        if (opfPath.equals(zipEntry.getName())) {
+          pa = JAXB.unmarshal(new ByteArrayInputStream(zipIn.readAllBytes()), Opf.class);
+          break;
         }
-        return pa;
+      }
     }
+    return pa;
+  }
 
-    private String getOpfPath(InputStream ebook) throws IOException {
-        String opfPath = null;
-        try (var zipIn = new ZipInputStream(ebook)) {
-            ZipEntry zipEntry;
-            while ((zipEntry = zipIn.getNextEntry()) != null) {
-                if ("META-INF/container.xml".equals(zipEntry.getName())) {
-                    var container = JAXB.unmarshal(new ByteArrayInputStream(zipIn.readAllBytes()), Container.class);
-                    opfPath = container.getRootfiles().getRootfile()[0].getFullPath();
-                    break;
-                }
-            }
+  private String getOpfPath(InputStream ebook) throws IOException {
+    String opfPath = null;
+    try (var zipIn = new ZipInputStream(ebook)) {
+      ZipEntry zipEntry;
+      while ((zipEntry = zipIn.getNextEntry()) != null) {
+        if ("META-INF/container.xml".equals(zipEntry.getName())) {
+          var container = JAXB.unmarshal(new ByteArrayInputStream(zipIn.readAllBytes()), Container.class);
+          opfPath = container.getRootfiles().getRootfile()[0].getFullPath();
+          break;
         }
-        return opfPath;
+      }
     }
+    return opfPath;
+  }
 
 }
