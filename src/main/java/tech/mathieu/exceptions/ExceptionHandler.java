e8cd8bcdad1b13635573f0992b0f02ae601564c4
Mathieu Keller <Business@Mathieu.tech>
2023-01-08 14:56:07 +0100

    fix: log exceptions (#35)

--- src/main/java/tech/mathieu/exceptions/ExceptionHandler.java
+++ src/main/java/tech/mathieu/exceptions/ExceptionHandler.java
@@ -4,12 +4,16 @@ import java.util.Objects;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
+import org.jboss.logging.Logger;
 
 @Provider
 public class ExceptionHandler implements ExceptionMapper<RuntimeException> {
 
+  private static final Logger LOGGER = Logger.getLogger(ExceptionHandler.class);
+
   @Override
   public Response toResponse(RuntimeException e) {
+    LOGGER.error(e.getMessage(), e);
     if (Objects.equals(e.getClass(), IllegalArgumentException.class)) {
       return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).build();
     }
