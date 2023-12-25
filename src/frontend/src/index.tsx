c29886bba200a8cfcbebdd8e848dfe322314816a
mathieu <mathieu.keller@sda.se>
2023-12-25 14:21:24 +0100

    fix(frontend): fix frontend router

--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -2,7 +2,25 @@
 import {render} from 'solid-js/web';
 
 import './index.css';
-import App from './App';
-import {Router} from '@solidjs/router';
+import {Route, Router} from '@solidjs/router';
+import {type Component, lazy} from 'solid-js';
+import Header from './Header/Header';
+import type {RouteSectionProps} from '@solidjs/router/dist/types';
 
-render(() => <Router><App/></Router>, document.getElementById('root') as HTMLElement);
+const Book = lazy(() => import('./Book/Book'));
+const Collection = lazy(() => import('./Collection/Collection'));
+const Library = lazy(() => import('./Library/Library'));
+
+const Search = lazy(() => import('./Search/Search'));
+const App: Component<RouteSectionProps> = (props: RouteSectionProps) => (
+    <>
+        <Header/>
+        {props.children}
+    </>
+);
+render(() => <Router root={App}>
+    <Route path="/collection/:collectionId/:collection" component={Collection}/>
+    <Route path="/book/:bookId/:book" component={Book}/>
+    <Route path="/search" component={Search}/>
+    <Route path="/" component={Library}/>
+</Router>, document.getElementById('root') as HTMLElement);
